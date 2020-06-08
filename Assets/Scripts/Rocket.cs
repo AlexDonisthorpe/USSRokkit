6023cb419a559bc22d926a451a8fe87ec242c4af
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-06-08 15:52:27 +0100

    Added thruster sound on boost button press + minor refactoring

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,33 +6,50 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Rotate()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up);
-            Debug.Log("Booster Active");
-        }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(Vector3.forward);
             Debug.Log("Rotating Left");
-        } else if (Input.GetKey(KeyCode.RightArrow)){
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
             transform.Rotate(Vector3.back);
             Debug.Log("Rotating Right");
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            Debug.Log("Booster Active");
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 }
