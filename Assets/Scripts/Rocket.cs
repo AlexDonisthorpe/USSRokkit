473b164e5abb60c81af8ba590d0c5a0a9f73641b
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-06-09 14:09:19 +0100

    Created second level & prefabbed the landing pad. Additionally added basic scene management to progress through the scenes prepared.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     //Variables
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 300f;
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
 
     // References
     Rigidbody rigidBody;
@@ -22,8 +26,11 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
 
     private void Rotate()
@@ -62,15 +69,38 @@ public class Rocket : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive)
+        {
+            return;
+        }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("OK");
                 break;
+            case "Finish":
+                Debug.Log("You win!");
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f); // parameterise time
+                break;
             default:
+                state = State.Transcending;
+                Invoke("LoadPreviousScene", 1f);
+                audioSource.Stop();
                 Debug.Log("Dead");
                 break;
         }
+    }
+
+    private void LoadPreviousScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); // allow for more than two levels
     }
 }
 
